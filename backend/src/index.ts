fe1316251df09d59681f84f7c9559f1f4409cfb9
Yao Xiao <xiao.yao5@northeastern.edu>
2021-04-21 23:09:50 -0700

    adding post and offer api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,17 +3,29 @@ import cors from "cors";
 import config from "./config";
 import dbInit from "./db-init";
 import { exampleRouter } from "./routers";
+import { carRouter } from "./routers/cars";
+import {offerRouter} from "./routers/offers"
+import {postRouter} from "./routers/posts"
 
 const serverInit = () => {
   const app = express();
 
   app.use(cors());
+  // add body-parser
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
   app.get("/", (request, response) => {
     response.send("Hello World");
   });
 
   app.use("/api/example", exampleRouter);
+  app.use("/api/cars", carRouter);
+
+  app.use("/api/offers",offerRouter);
+  app.use("/api/posts",postRouter);
+
 
   app.listen(config.port, () => {
     console.log(`The server is started at port ${config.port}`);
